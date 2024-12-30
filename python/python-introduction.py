4f249a1edde165dd2263e97f13d67624d3790118
Killian Coffey <killiancoffey@clarincollege.ie>
2024-12-30 18:35:40 +0000

    first commit killian

---
+++ python/python-introduction.py
@@ -0,0 +1,38 @@
+#print("Hello, World!")
+#print("ronaldo, legend")
+
+#age = 12
+#print(age)
+
+#input("what age are you?")
+
+#Print("Hello!")
+#print "Hello!"
+#print("Hello!")
+
+#let age = :12
+#age = 12
+#Var age = ==12
+
+#// This is a comment
+#The is a comment */
+# This is a comment
+
+#input("Enter your name")
+#input "Enter your name"
+#get_input( “Enter your name”)
+
+#if a = 5:
+#if a == 5:
+#if a equals 5:
+
+# Q13 to write a program to check if a number is greater than 10.
+
+num = 5
+num = int(input("Please enter a number: "))
+
+if num > 10:
+    print ("The number is big!")
+else:
+    print ("The number is small!")
+
